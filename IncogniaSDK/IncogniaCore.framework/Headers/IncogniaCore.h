36eea8178f867fc279363115803152aae86db13d
Pedro Atanasio <pedro.atanasio@inloco.com.br>
2020-06-15 17:06:20 -0300

    feat(incognia-pods): updates frameworks

---
+++ IncogniaSDK/IncogniaCore.framework/Headers/IncogniaCore.h
@@ -0,0 +1,20 @@
+//
+//  Header.h
+//  IncogniaCore
+//
+//  Created by Pedro Atanasio on 04/06/20.
+//  Copyright © 2020 Incognia. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <IncogniaCore/ICGError.h>
+#import <IncogniaCore/ICGOptions.h>
+#import <IncogniaCore/ICGConsentTypes.h>
+#import <IncogniaCore/ICGConsentResult.h>
+#import <IncogniaCore/ICGConsentDialogOptions.h>
+#import <IncogniaCore/ICGUserAddress.h>
+#import <IncogniaCore/ICGCheckIn.h>
+
+#endif /* Header_h */
