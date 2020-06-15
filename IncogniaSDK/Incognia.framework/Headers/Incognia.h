bfb39be4e60efaeeacab661dd6c9b8f85010ca03
Pedro Atanasio <pedro.atanasio@inloco.com.br>
2020-06-15 13:24:54 -0300

    feat(cocoapods): adds preliminary frameworks

---
+++ IncogniaSDK/Incognia.framework/Headers/Incognia.h
@@ -0,0 +1,17 @@
+//
+//  Incognia.h
+//  Incognia
+//
+//  Created by Pedro Atanasio on 05/06/20.
+//  Copyright © 2020 Incognia. All rights reserved.
+//
+
+#ifndef Incognia_h
+#define Incognia_h
+
+#import <Incognia/ICGIncognia.h>
+
+#import <IncogniaCore/ICGError.h>
+#import <IncogniaCore/ICGUserAddress.h>
+
+#endif /* Incognia_h */
